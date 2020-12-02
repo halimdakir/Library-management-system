f86b639f5eb1a1faec05e05e54b71f1b8775e51b
Halimove01 <halim.dakir@gmail.com>
2020-12-02 09:58:42 +0100

    add jsf frontend

--- src/main/java/se/iths/library/LibraryManagementSystemApplication.java
+++ src/main/java/se/iths/library/LibraryManagementSystemApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.ServletContext;
 import java.util.Arrays;
@@ -21,7 +20,6 @@ public class LibraryManagementSystemApplication {
     ServletRegistrationBean jsfServletRegistration (ServletContext servletContext) {
         //spring boot only works if this is set
         servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
-
         //FacesServlet registration
         ServletRegistrationBean srb = new ServletRegistrationBean();
         srb.setServlet(new FacesServlet());

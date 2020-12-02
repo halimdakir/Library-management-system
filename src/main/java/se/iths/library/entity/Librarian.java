9408185b9f7ecbcccaeb8449ec05f0469fc980e8
Halimove01 <halim.dakir@gmail.com>
2020-12-02 11:12:43 +0100

    added Librarian entity

--- src/main/java/se/iths/library/entity/Librarian.java
+++ src/main/java/se/iths/library/entity/Librarian.java
@@ -1,5 +1,48 @@
 package se.iths.library.entity;
 
-public class Librarian {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
 
+@Entity
+public class Librarian {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private Date dateOfBirth;
+
+    public Librarian() {
+    }
+
+    public Librarian(String fullName, Date dateOfBirth) {
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }

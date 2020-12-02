e69d42b36bb68f5ce71a900fb7d0fd334d646b82
Halimove01 <halim.dakir@gmail.com>
2020-12-02 11:18:20 +0100

    added item lending entity

---
+++ src/main/java/se/iths/library/entity/Item_Lending.java
@@ -0,0 +1,56 @@
+package se.iths.library.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
+@Entity
+public class Item_Lending {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotEmpty
+    private Date creationDate;
+    @NotEmpty
+    private Date dueDate;
+    @NotEmpty
+    private Date returnDate;
+
+    public Item_Lending() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+}

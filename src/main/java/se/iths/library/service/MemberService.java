3d47ebb8216fa59f6d23ceb84a3bc1b01d74d65c
Halimove01 <halim.dakir@gmail.com>
2020-12-02 11:52:32 +0100

    added member service

---
+++ src/main/java/se/iths/library/service/MemberService.java
@@ -0,0 +1,33 @@
+package se.iths.library.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import se.iths.library.entity.Member;
+import se.iths.library.repository.MemberRepository;
+
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Member createMember(Member member){
+        return memberRepository.save(member);
+    }
+    public void deleteMemberById(Long id){
+        Optional<Member> foundMember = memberRepository.findById(id);
+        memberRepository.deleteById(foundMember.get().getId());
+    }
+    public Optional<Member> getMemberById(Long id){
+        return memberRepository.findById(id);
+    }
+    public Iterable<Member> getAllMember(){
+        return memberRepository.findAll();
+    }
+}

b5b1f9be3dba2d90de4c30c2fb45cd2d00757452
Halimove01 <halim.dakir@gmail.com>
2020-12-07 20:36:38 +0100

    added stock service

--- src/main/java/se/iths/library/service/StockService.java
+++ src/main/java/se/iths/library/service/StockService.java
@@ -2,7 +2,11 @@ package se.iths.library.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.iths.library.entity.Library;
+import se.iths.library.entity.Stock;
 import se.iths.library.repository.StockRepository;
+
+import java.util.Optional;
 
 @Service
 public class StockService {
@@ -12,5 +16,7 @@ public class StockService {
     public StockService(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
-
+    public Stock createStock(Stock stock){
+        return stockRepository.save(stock);
+    }
 }

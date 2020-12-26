285991d4df245db036d7ad218882e1f22b95c2c4
Halimove01 <halim.dakir@gmail.com>
2020-12-26 18:30:29 +0100

    Added authentication response

--- src/main/java/se/iths/library/securityJwt/AuthenticationResponse.java
+++ src/main/java/se/iths/library/securityJwt/AuthenticationResponse.java
@@ -1,5 +1,13 @@
 package se.iths.library.securityJwt;
 
 public class AuthenticationResponse {
+    private final String jwt;
 
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
 }
